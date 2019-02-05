2608d1adf360cda779d8f6e19b537c3cbb9fe370
Kishan Patel <kishan02patel@gmail.com>
2019-02-06 00:14:26 +0530

    Created basic server to listen to socket

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const socket = require('socket.io')
+const cors = require('cors')
+const app = express()
+const PORT = 3001
+
+app.use(cors())
+
+server = app.listen(PORT, () => {
+	console.log('Server started on port:', PORT)
+	io = socket(server)
+	io.on('connection', (socket) => {
+		console.log('Someone Connected with socket id', socket.id)
+	})
+})
