72bf684e3c859bc99b9b65f7680871051817ff70
Kishan Patel <kishan02patel@gmail.com>
2019-02-07 18:44:27 +0530

    Prevent users from going back to editor page once session is destroyed

--- src/session-page.js
+++ src/session-page.js
@@ -1,38 +1,52 @@
 import React from 'react'
 import { SERVER_URL } from './config/config'
 
-const SessionPage = (props) => {
+class SessionPage extends React.Component {
+	constructor(props) {
+		super(props)
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.createSession = this.createSession.bind(this)
+	}
 
-	function handleSubmit(event) {
+	componentDidMount() {
+		// Making sure that user is not able to go forward again to the editor page once session is destroyed. 
+		window.history.pushState(null, document.title, window.location.href);
+		window.addEventListener('popstate', function (event) {
+			window.history.pushState(null, document.title, window.location.href);
+		});
+	}
+
+	handleSubmit(event) {
 		event.preventDefault()
 		const url = event.target[0].value
 		if (url.length === 0)
 			alert('This cannot be empty. Either create new session or paste the URL here.')
 		else {
+			// Check whether the socket namespace exists or not. 
 			fetch(`${SERVER_URL}/checkSocketExists?url=${url}`)
 				.then(response => response.json())
 				.then(response => {
 					if (response.isSocketPresent) {
-						props.history.push({
+						this.props.history.push({
 							pathname: '/editor-page',
 							state: { url }
 						})
 					}
 					else {
 						if (window.confirm('This URL does not exist. Do you want to create a new session?')) {
-							createSession()
+							this.createSession()
 						}
 					}
 				})
 		}
 	}
 
-	function createSession() {
+	createSession() {
 		fetch(`${SERVER_URL}/getSessionURL`)
 			.then(response => response.json())
 			.then(response => {
 				var url = response.url
-				props.history.push({
+				this.props.history.push({
 					pathname: '/editor-page',
 					state: { url }
 				})
@@ -40,18 +54,20 @@ const SessionPage = (props) => {
 			.catch(err => console.log(err))
 	}
 
-	return (
-		<div>
-			<button onClick={createSession}>Create New Session</button>
-			<br /><br />
-			<h3>OR</h3>
-			<br />
-			<form onSubmit={handleSubmit}>
-				<input type="text" placeholder="Type the URL to connect..." />
-				<input type="submit" value="Connect" />
-			</form>
-		</div>
-	)
+	render() {
+		return (
+			<div>
+				<button onClick={this.createSession}>Create New Session</button>
+				<br /> <br />
+				<h3>OR</h3>
+				<br />
+				<form onSubmit={this.handleSubmit}>
+					<input type="text" placeholder="Type the URL to connect..." />
+					<input type="submit" value="Connect" />
+				</form>
+			</div >
+		)
+	}
 }
 
 export default SessionPage
