dea876ed1dec7c882e4d16e259fccfe29accbb44
Kishan Patel <kishan02patel@gmail.com>
2019-02-07 18:06:42 +0530

    Redirecting the user if user directly opens this page

--- src/editor-page.js
+++ src/editor-page.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import io from "socket.io-client";
 import Editor from './editor'
 import { SERVER_URL } from './config/config'
+import { Redirect } from 'react-router-dom'
 
 class EditorPage extends Component {
 	constructor(props) {
 		super(props)
 		this.handleChange = this.handleChange.bind(this)
-		console.log(props.location.state.url)
-		this.socket = io(`${SERVER_URL}/${props.location.state.url}`)
+		this.componentWillMount = this.componentWillMount.bind(this)
+		this.socket = null
+	}
+
+	componentWillMount() {
+		// Check whether this page is opened via the previous session page and the state(and url) is passed or not. If it is passed then create socket to listen on the passed URL.
+		this.props.location.state ? this.socket = io(`${SERVER_URL}/${this.props.location.state.url}`) : console.log('Session not estiblished')
 	}
 
 	handleChange(data) {
@@ -16,24 +22,30 @@ class EditorPage extends Component {
 	}
 
 	componentWillUnmount() {
-		console.log('component unmounted')
-		this.socket.emit('LEAVE_SESSION')
-		this.socket.close()
+		// Check whether the session is created via previous page or not.
+		if (this.props.location.state) {
+			// Before navigating to another page send a msg to server for disconnection.
+			this.socket.emit('LEAVE_SESSION')
+			this.socket.close()
+		}
 	}
 
 	render() {
 		return (
-			<div>
-				<p>
-					Share <span onClick={
-						function (event) {
-							navigator.clipboard.writeText(event.target.innerHTML);
-							document.execCommand("copy")
-						}
-					} title="Click to copy"><u><b>{this.props.location.state.url}</b></u></span> with your peers to work together.
+			// Check whether the session is created via previous page or not. If session is not created then redirect the user to session page else display this component
+			!this.props.location.state ? <div>{< Redirect to='/' />}</div> :
+				<div>
+					<p>
+						{/* Create a span tag that will show the socket namespace which can be shared with others to connect to same socket namespace and work together. The onclick function is set up to copy this reference when user clicks on this.  */}
+						Share <span onClick={
+							function (event) {
+								navigator.clipboard.writeText(event.target.innerHTML);
+								document.execCommand("copy")
+							}
+						} title="Click to copy"><u><b>{this.props.location.state.url}</b></u></span> with your peers to work together.
 				</p>
-				<Editor handleChange={this.handleChange} socket={this.socket} />
-			</div >
+					<Editor handleChange={this.handleChange} socket={this.socket} />
+				</div >
 		);
 	}
 }
