17938a5b2388d8da39d17732964657f2ea94659c
Kishan <kishan02patel@gmail.com>
2019-05-16 23:36:28 +1000

    Refactor server code to serve index page

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+const PORT = process.env.PORT || 8000
 
-const PORT = process.env.PORT || 3000
-
-app.use(express.static(path.resolve(__dirname, 'build')))
+app.use(express.static('build'))
 
 app.get('*', (req, res) => {
-	res.sendFile('index.html')
+	res.redirect('/')
 })
 
-app.listen(PORT, () => console.log('React server started on port:', PORT))
+app.listen(PORT, () => console.log('Server started on port ', PORT))
