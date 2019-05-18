2cb29471f687b110f60c0038ccf88b1781d9e966
Kishan <kishan02patel@gmail.com>
2019-05-18 10:26:04 +1000

    Change env var for server URL

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000'
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3000'
 
 module.exports = {
 	SERVER_URL
